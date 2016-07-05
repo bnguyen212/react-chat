05a5eebefa6ff543065c404afb76c1389d97f484
Mustafa Paksoy <paksoy@gmail.com>
2016-07-05 19:18:46 -0400

    Add mockups.

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 ## Goal
 
+The goal of this exercise
+
 ## Instructions
 
 ### Part 1: Chat component
+
+![](img/chat1.png)
+
+`<App />`
+
+`<ChatRoom />`
+- Send message form
+- Message list view
+
+### Part 2: Multi-room chat
+
+![](img/chat2.png)
+
+<ChatRoomSelector />
+
+- Add required `name` prop for `ChatRoom`
+- `componentDidMount()` on ChatRoom join channel
+- `componentWillUnmount()` disconnects/leaves channel
+
+### Bonus part: "User is typing"
+
+![](img/chat3.png)
--- client/index.js
+++ client/index.js
@@ -3,13 +3,23 @@ var ReactDOM = require('react-dom');
 
 var App = React.createClass({
   componentDidMount: function() {
-    var socket = io();
-    socket.on('connect', function() {
+    this.socket = io();
+    this.socket.on('connect', function() {
       console.log('connected');
     });
   },
+  join: function(room) {
+    if (! room) {
+      throw new ('Room missing');
+    }
+  },
   render: function() {
-    return <h1>Test</h1>;
+    return (
+      <div>
+        <h1>Test</h1>
+        <button className="btn btn-default" onClick={this.join.bind(this, 1)}>Join room 1</button>
+      </div>
+    );
   }
 });
 
