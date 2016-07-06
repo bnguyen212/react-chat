ac02eee886d7ea5afaf744cd8ff8562938df749c
Isaac Kapadia <ikapedia@umich.edu>
2016-07-06 15:50:20 -0400

    working

--- client/index.js
+++ client/index.js
@@ -5,25 +5,29 @@ var App = React.createClass({
   getInitialState: function() {
     return {
       socket: io(),
-      // YOUR CODE HERE (1)
+      roomName: 'none selected',
     }   
   },
   componentDidMount: function() {
-
     // WebSockets Receiving Event Handlers
     this.state.socket.on('connect', function() {
       console.log('connected');
-      // YOUR CODE HERE (2)
+      this.setState({
+        username: prompt('enter a username')
+      })
+      this.state.socket.emit('username', this.state.username);
     }.bind(this));
-
     this.state.socket.on('errorMessage', function(message) {
-      // YOUR CODE HERE (3)
+      alert(message);
     }.bind(this));
-
   },
   join: function(room) {
     // room is called with "Party Place"
     console.log(room);
+    this.setState({
+      roomName: room
+    })
+   this.state.socket.emit('room', this.state.roomName)
   },
   render: function() {
     return (
@@ -32,9 +36,43 @@ var App = React.createClass({
         <button className="btn btn-default" onClick={this.join.bind(this, "Party Place")}>
           Join the Party Place
         </button>
+        <ChatRoom username={this.state.username} socket={this.state.socket} roomName={this.state.roomName}/>
       </div>
     );
   }
 });
+
+var ChatRoom = React.createClass({
+  getInitialState: function() {
+    return {    
+      message: '',
+      messages: []
+    }
+  },
+  componentDidMount: function() {
+    this.props.socket.on('messages', function(messages) {
+      this.setState({
+        messages: messages
+      })
+    } )
+  },
+  componentWillReceiveProps: function(nextProps) {
+    if (nextProps.roomName !== this.props.roomName) {
+      this.props.socket.emit('room', nextProps.roomName);
+      this.setState({
+        roomName: nextProps.roomName,
+        messages: []
+      })
+    }
+  },
+  render: function() {
+    // this.props.socket,
+    // this.props.roomName
+    
+    return this.state.message.map(function(message) {
+      
+    });
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
