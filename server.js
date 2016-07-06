8103d3d9ad931bbcf0ec93d6ce6375e05b729795
Ethan Lee <ethanlee@ethansites.com>
2016-07-06 05:02:32 -0400

    Add complete WS chat server

--- server.js
+++ server.js
@@ -24,6 +24,38 @@ app.get('/', function(req, res) {
 
 io.on('connection', function (socket) {
   console.log('connected');
+  socket.on('username', function(username) {
+    if (!username.trim()) {
+      return socket.emit('errorMessage', 'No username!');
+    }
+    socket.username = String(username);
+  });
+
+  socket.on('room', function(requestedRoom) {
+    if (!socket.username) {
+      return socket.emit('errorMessage', 'Username not set!');
+    }
+    if (!requestedRoom) {
+      return socket.emit('errorMessage', 'No room!');
+    }
+    socket.room = requestedRoom;
+    socket.join(requestedRoom, function() {
+      socket.to(requestedRoom).emit('message', {
+        username: 'System',
+        content: socket.username + ' has joined'
+      });
+    });
+  });
+
+  socket.on('message', function(message) {
+    if (!socket.room) {
+      return socket.emit('errorMessage', 'No rooms joined!');
+    }
+    socket.to(socket.room).emit('message', {
+      username: socket.username,
+      content: message
+    });
+  })
 });
 
 var port = process.env.PORT || 3000;
