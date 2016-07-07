6d059a0a2c23bc8a6f4a914fedaacb90ff73a745
Lucas <lmandelbaum1@babson.edu>
2016-07-06 21:02:28 -0400

    not done

--- client/index.js
+++ client/index.js
@@ -5,19 +5,20 @@ var App = React.createClass({
   getInitialState: function() {
     return {
       socket: io(),
-      // YOUR CODE HERE (1)
+      roomName: "No room selected!",
     }   
   },
   componentDidMount: function() {
-
+    var username = "Lucas";
     // WebSockets Receiving Event Handlers
     this.state.socket.on('connect', function() {
       console.log('connected');
+      this.state.socket.username = username;
       // YOUR CODE HERE (2)
     }.bind(this));
 
     this.state.socket.on('errorMessage', function(message) {
-      // YOUR CODE HERE (3)
+        alert(message)
     }.bind(this));
 
   },
@@ -32,9 +33,53 @@ var App = React.createClass({
         <button className="btn btn-default" onClick={this.join.bind(this, "Party Place")}>
           Join the Party Place
         </button>
+        <ChatRoom name={this.state.roomName} socket={this.state.socket}/>
       </div>
     );
   }
+});
+
+var ChatRoom = React.createClass({
+  getInitialState: function(){
+    return {
+      message: '',
+      messages: []
+    }
+  },
+  recieveMessage: function(message) {
+    this.setState({
+      messages: this.state.messages.concat([message])
+    })
+  },
+  componentDidMount: function(){
+    alert(this.props.socket.message);
+    this.props.socket.on('message', this.receiveMessage)
+  },
+  // componentWillReceiveProps: function(nextProps){
+  //   this.props.socket.on('room', function (e) {
+  //         e.preventDefault();
+  //         socket.emit('room', {
+  //             name: nextProps.name
+  //         });
+  //         // socket.on('my other event', function (data) {
+  //         //   console.log(data);
+  //         // });
+  //       });
+  //   if (this.props.name != nextProps.name) {
+  //     this.setState({
+  //       roomName: nextProps.name,
+  //       messages: []
+  //     })
+  //   } 
+  // },
+  render: function(){
+    return (
+      <div>
+
+      </div>
+      )
+  }
+
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
